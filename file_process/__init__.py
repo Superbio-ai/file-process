82258b5b0d3d071866aaba09dfd487ebc79755f7
Kateryna Dubinina <ururruu@gmail.com>
2022-11-24 14:11:15 +0200

    change to kwargs

--- file_process/__init__.py
+++ file_process/__init__.py
@@ -4,8 +4,8 @@ from file_process.file_processor_factory import FileProcessFactory
 
 
 # pylint: disable=too-many-function-args
-def preview_file(filename: str, file, *, job_id: Optional[str] = None, delimiter: Optional[str] = None) \
+def preview_file(filename: str, file, *, model_csv_file=None, **kwargs) \
         -> (List[str], Optional[List[dict]], Optional[List[dict]]):
     processor = FileProcessFactory.get(filename)
-    target_names, var_preview, obs_preview = processor.process(file, job_id, delimiter)
+    target_names, var_preview, obs_preview = processor.process(file, model_csv_file, **kwargs)
     return target_names, processor.create_tabular_response(var_preview), processor.create_tabular_response(obs_preview)
