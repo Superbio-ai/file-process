7616bfac8caa597e6ba9048fab5780e5ec84b0d3
Kateryna Dubinina <ururruu@gmail.com>
2022-12-08 17:19:48 +0200

    remove h5

--- tests/test_file_process/test_file_processor_factory.py
+++ tests/test_file_process/test_file_processor_factory.py
@@ -3,17 +3,17 @@ import pytest
 from file_process.csv import CSVFileProcessor
 from file_process.exceptions import WrongExtension
 from file_process.file_processor_factory import FileProcessFactory
-from file_process.h5 import H5FileProcessor
+from file_process.h5 import H5ADFileProcessor
 
 
 class TestFileProcessFactory:
     def test_get_h5_processor(self):
         res = FileProcessFactory.get('heart_atlas.h5ad')
-        assert isinstance(res, H5FileProcessor)
+        assert isinstance(res, H5ADFileProcessor)
 
     def test_get_h5ad_processor(self):
-        res = FileProcessFactory.get('heart_atlas.h5')
-        assert isinstance(res, H5FileProcessor)
+        with pytest.raises(WrongExtension):
+            _ = FileProcessFactory.get('heart_atlas.h5')
 
     def test_get_csv_processor(self):
         res = FileProcessFactory.get('heart_atlas.csv')
