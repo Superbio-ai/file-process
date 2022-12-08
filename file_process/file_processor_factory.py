023934c36bbbf6e23087296744b7d977a41ee7ef
Kateryna Dubinina <ururruu@gmail.com>
2022-12-08 16:44:02 +0200

    add read h5 10x

--- file_process/file_processor_factory.py
+++ file_process/file_processor_factory.py
@@ -1,13 +1,18 @@
 from file_process.csv import CSVFileProcessor
 from file_process.exceptions import WrongExtension
-from file_process.h5 import H5FileProcessor
+from file_process.h5 import H5FileProcessor, H510xFileProcessor
 
 
 class FileProcessFactory:  # pylint: disable=too-few-public-methods
     @classmethod
-    def get(cls, filename: str):
+    def get(cls, filename: str, file):
         if filename.endswith('.h5') or filename.endswith('.h5ad'):
-            return H5FileProcessor()
+            h5_processor = H5FileProcessor()
+            try:
+                _ = h5_processor.read_file(file)
+                return h5_processor
+            except TypeError as e:
+                return H510xFileProcessor()
         if filename.endswith('.csv'):
             return CSVFileProcessor()
         raise WrongExtension
--- file_process/h5.py
+++ file_process/h5.py
@@ -1,4 +1,8 @@
+from io import BytesIO
+
 import anndata
+import scanpy as sc
+from werkzeug.datastructures import FileStorage
 
 from file_process.base import TabularFileProcessorBase
 
@@ -7,3 +11,12 @@ class H5FileProcessor(TabularFileProcessorBase):
 
     def read_file(self, file, **kwargs):
         return anndata.read_h5ad(file)
+
+
+class H510xFileProcessor(TabularFileProcessorBase):
+
+    def read_file(self, file, **kwargs):
+        if isinstance(file, FileStorage):  # TODO try to get rid of it
+            file = file.read()
+            # file = BytesIO(file)
+        return sc.read_10x_h5(file)
