0ddcceabe648a6979986877d5372f1cfb2ba1f7d
Kateryna Dubinina <ururruu@gmail.com>
2022-12-08 16:52:10 +0200

    change code

--- file_process/file_processor_factory.py
+++ file_process/file_processor_factory.py
@@ -5,9 +5,16 @@ from file_process.h5 import H5FileProcessor
 
 class FileProcessFactory:  # pylint: disable=too-few-public-methods
     @classmethod
-    def get(cls, filename: str):
+    def get(cls, filename: str, file):
         if filename.endswith('.h5') or filename.endswith('.h5ad'):
-            return H5FileProcessor()
+            h5_processor = H5FileProcessor()
+            try:
+                # File extension alone is not enough to determine the read method:
+                # sometimes h5-10x files may have .h5 extension.
+                _ = h5_processor.read_file(file)
+                return h5_processor
+            except TypeError as exc:
+                raise WrongExtension from exc
         if filename.endswith('.csv'):
             return CSVFileProcessor()
         raise WrongExtension
