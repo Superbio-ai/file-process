a955777e85256624d3a8fe4954117613881ebc5a
Kateryna Dubinina <ururruu@gmail.com>
2022-12-15 17:20:41 +0200

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="file_process",
-      version="1.0.2",
+      version="1.0.3",
       description="A package that does file validation and file preview.",
       license='MIT',
       author="superbio.ai",
