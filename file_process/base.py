88d9d97ff82de0f340ce71f4dc3665bc655b4bba
Kateryna Dubinina <ururruu@gmail.com>
2023-02-06 15:53:53 +0200

    comment base class

--- file_process/base.py
+++ file_process/base.py
@@ -5,10 +5,11 @@ from file_process.h5ad.schemas import SbioModelData
 
 
 class FileProcessorBase(ABC):
-    @abstractmethod
-    def validate(self, model_data: SbioModelData):
-        pass
-
-    @abstractmethod
-    def get_preview(self) -> (List[str], List[dict], List[dict]):
-        pass
+    pass
+    # @abstractmethod
+    # def validate(self, model_data: SbioModelData):
+    #     pass
+    #
+    # @abstractmethod
+    # def get_preview(self) -> (List[str], List[dict], List[dict]):
+    #     pass
