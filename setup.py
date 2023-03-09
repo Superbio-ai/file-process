8806fcbbc5ab870b2a222a938c500e8119f03457
Kateryna Dubinina <ururruu@gmail.com>
2023-03-09 17:35:43 +0200

    Fix x validation, add positive tests for h5ad validation (#24)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="file_process",
-      version="1.2.2",
+      version="1.2.3",
       description="A package that does file validation and file preview.",
       license='MIT',
       author="superbio.ai",
