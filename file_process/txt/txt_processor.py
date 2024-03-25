a100817eecc584f3ce526043a8e56011f2b80209
drogo <dmason@superbio.ai>
2024-03-25 10:36:00 +0000

    clean up for pylint

--- file_process/txt/txt_processor.py
+++ file_process/txt/txt_processor.py
@@ -1,16 +1,7 @@
-from typing import List, Optional
+from typing import Optional
 from io import BytesIO
-import csv
-
-import pandas as pd
-from numpy import number, nan
-from pandas.errors import ParserError
 
 from file_process.base import FileProcessorBase
-from file_process.constants import PREVIEW_ROWS_COUNT
-from file_process.csv.csv_validator import CSVValidator
-from file_process.csv.schemas import SbioModelDataForCsv
-from file_process.exceptions import DelimiterError
 
 
 class TxtFileProcessor(FileProcessorBase):
