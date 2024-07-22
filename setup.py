f9b5f87b3a323579ee07b6f0cc45a44439687beb
Oleh Ivashchenko <olehivashchenko94@gmail.com>
2024-07-22 19:11:38 +0300

    remove scanpy dependency copy removed code from source code

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name="file_process",
       license='MIT',
       author="superbio.ai",
       url='https://github.com/Superbio-ai/file-process',
-      install_requires=['pandas==1.2.5', 'anndata==0.8.0', 'scanpy==1.9.1', 'numpy>=1.22'],
+      install_requires=['pandas==1.2.5', 'anndata==0.8.0', 'numpy>=1.22'],
       packages=['file_process', 'file_process.h5ad', 'file_process.csv', 'file_process.txt'])
