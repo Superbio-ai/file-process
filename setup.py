eb4e6c6ae73a8ed8c3f700fdda4c7442027874ec
Oleh Ivashchenko <olehivashchenko94@gmail.com>
2024-07-22 15:24:57 +0300

    add long description

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
 
 setup(name="file_process",
-      version="1.4.1",
+      version="1.4.2",
       description="A package that does file validation and file preview.",
+      long_description=README,
+      long_description_content_type="text/markdown",
       license='MIT',
       author="superbio.ai",
       url='https://github.com/Superbio-ai/file-process',
